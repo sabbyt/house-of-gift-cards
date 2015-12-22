abc33e5bfef4b1acf702adcbb3e77fac4c3d1bef
Stephen Carney <stephencarney1990@gmail.com>
2015-12-22 10:24:26 -0800

    implemetning template 2

--- scripts/request-form.js
+++ scripts/request-form.js
@@ -10,40 +10,14 @@ giftWall.getTemplate = function() {
   giftWall.template = Handlebars.compile(appTemplate);
 };
 
-
 giftWall.getTemplate();
-giftWall.toHTML(JSON.parse({ "request_id": "1",
-    "request_dt": "2015-11-29 13:02:12 GMT-0800 (PST)",
-    "first_name": "Wanda",
-    "last_name": "Maximoff",
-    "age": "100",
-    "email": "wanda@hotmail.com",
-    "story": "I help to cook at the soup kitchen once a month.",
-    "category": "Food",
-    "brand": "QFC",
-    "amount": "20",
-    "status": "UNCLAIMED"
-  }));
 
+var ref = new Firebase('https://hogc.firebaseio.com/requests');
 
+ref.once('value', function(data) {
+  data.val().forEach(giftWall.toHTML);
+});
 
-// var Request = function (object) {
-//   this.first_name = object.first_name;
-//   this.story = object.story;
-//   this.category = object.category;
-//   this.brand = object.brand;
-//   this.amount = object.amount;
-// };
-//
-// requests.newRequest = function() {
-//   for (var i = 0; i < requests.requestData.length; i++) {
-//     var requestEntry = new Request(requests.requestData[i]);
-//     this.appendRequest(requestEntry);
-//   }
-// };
-//
-// requests.appendRequest = function(request) {
-//   $('#entry').append(request.toHTML());
-// };
+//truncate story content
 
-//toHTML function
+//function for popup
