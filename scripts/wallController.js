decd41eac451a83179ac7cd94e36a14aa932d20e
Natalie Chow <cychownat@gmail.com>
2015-12-22 12:59:34 -0800

    Save current list of selection to local storage

--- scripts/wallController.js
+++ scripts/wallController.js
@@ -1,21 +1,34 @@
 var wallController = {};
+wallController.claimed = [];
 
 wallController.listView = function() {
   giftWall.getListTemplate(wallView.renderListAll);
 };
 
+wallController.handleClaimButtons = function() {
+  $('#entry').on('click', '.claim-button', function(event) {
+    event.preventDefault();
+    console.log('claimed' + $(this).data('key'));
+    $(this).text('Unclaim').removeClass('claim-button').addClass('unclaim-button');
+    $(this).parent().parent('tr').toggleClass('selected');
+
+    wallController.claimed.push($(this).data('key'));
+    console.log(wallController.claimed);
+    localStorage.setItem('claimed-keys', wallController.claimed);
+  });
+
+  $('#entry').on('click', '.unclaim-button', function(event) {
+    event.preventDefault();
+    console.log('unclaimed' + $(this).data('key'));
+    $(this).text('Claim').removeClass('unclaim-button').addClass('claim-button');
+    $(this).parent().parent('tr').toggleClass('selected');
+
+    var index = wallController.claimed.indexOf($(this).data('key'));
+    wallController.claimed.splice(index, 1);
+    console.log(wallController.claimed);
+    localStorage.setItem('claimed-keys', wallController.claimed);
+  });
+};
+
 wallController.listView();
-
-$('#entry').on('click', '.claim-button', function(event) {
-  event.preventDefault();
-  console.log('claimed' + $(this).data('key'));
-  $(this).text('Unclaim').removeClass('claim-button').addClass('unclaim-button');
-  $(this).parent().parent('tr').toggleClass('selected');
-});
-
-$('#entry').on('click', '.unclaim-button', function(event) {
-  event.preventDefault();
-  console.log('unclaimed' + $(this).data('key'));
-  $(this).text('Claim').removeClass('unclaim-button').addClass('claim-button');
-  $(this).parent().parent('tr').toggleClass('selected');
-});
+wallController.handleClaimButtons();
