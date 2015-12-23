548cbb0ac362b6b2dca661103470beb79ed25888
Natalie Chow <cychownat@gmail.com>
2015-12-23 12:14:03 -0800

    Updating firebase with newly registered user

--- scripts/login.js
+++ scripts/login.js
@@ -19,6 +19,7 @@ login.init = function() {
   console.log('init complete');
 };
 
+
 login.handleSignIn = function() {
   $('#sign-in-submit').on('click', function(event) {
     event.preventDefault();
@@ -29,29 +30,6 @@ login.handleSignIn = function() {
     }
   });
 };
-
-login.handleRegister = function() {
-  $('#register-submit').on('click', function(event) {
-    event.preventDefault();
-    $('#register-warning').hide();
-    var data = login.checkRegister();
-    if (data) {
-      // set password
-      // insert to firebase
-      // save to LS
-      // redirect
-    }
-  });
-};
-
-login.checkUserExist = function(usernameInput) {
-  var usernameList = login.userLibrary.map(function(donor) {
-    return donor.username;
-  });
-  return usernameList.indexOf(usernameInput);
-};
-
-login.showLogin();
 
 login.checkSignIn = function() {
   var usernameInput = $('#sign-in-username').val();
@@ -64,16 +42,35 @@ login.checkSignIn = function() {
         console.log('login success');
         return usernameInput;
       } else {
-        $('#sign-in-warning').show().text('Incorrect password');
+        login.signInWarning('Incorrect password');
       }
     } else {
-      $('#sign-in-warning').show().text('User does not exist');
+      login.signInWarning('User does not exist');
     }
   } else {
-    $('#sign-in-warning').show().text('Please enter a username');
+    login.signInWarning('Please enter a username');
   }
   return false;
 };
+
+
+
+login.handleRegister = function() {
+  $('#register-submit').on('click', function(event) {
+    event.preventDefault();
+    $('#register-warning').hide();
+    login.newUserData = login.checkRegister();
+    if (login.newUserData) {
+      login.registerSetPassword();
+      // set password
+      // insert to firebase
+
+      // save to LS
+      // redirect
+    }
+  });
+};
+
 
 login.checkRegister = function() {
   var fName = $('#register-fname').val();
@@ -84,18 +81,63 @@ login.checkRegister = function() {
   if (fName.length && lName.length && email.length && org.length) {
     if (usernameInput.length) {
       if (login.checkUserExist(usernameInput) > -1) {
-        $('#register-warning').show().text('Please enter a username');
+        login.registerWarning('Username already exists');
       } else {
-        return {};
+        var data = {
+          username: usernameInput,
+          first_name: fName,
+          last_name: lName,
+          organization: org,
+          email: email
+        };
+        return data;
       }
     } else {
-      $('#register-warning').show().text('Please enter a username');
+      login.registerWarning('Please enter a username');
     }
   } else {
-    $('#register-warning').show().text('All fields are required.');
+    login.registerWarning('All fields are required.');
   }
   return false;
 };
 
+
+login.checkUserExist = function(usernameInput) {
+  var usernameList = login.userLibrary.map(function(donor) {
+    return donor.username;
+  });
+  return usernameList.indexOf(usernameInput);
+};
+
+login.signInWarning = function(msg) {
+  $('#sign-in-warning').show().text(msg);
+};
+
+login.registerWarning = function(msg) {
+  $('#register-warning').show().text(msg);
+};
+
+
+login.registerSetPassword = function() {
+  $('#register-password-input').show();
+  $('#register input:not(#register-password)').attr('disabled', true);
+  $('#register-submit').text('Complete registration').on('click', function(event) {
+    event.preventDefault();
+    var passwordInput = $('#register-password').val();
+    if (passwordInput.length) {
+      login.newUserData.password = passwordInput;
+      login.registerFirebase();
+    } else {
+      login.registerWarning('Please enter a password');
+    }
+  });
+};
+
+login.registerFirebase = function() {
+  login.ref.push(login.newUserData);
+  console.log(login.newUserData);
+};
+
+login.showLogin();
 
 // show forms only after query is done
