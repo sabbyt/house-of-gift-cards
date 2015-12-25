f9d0a8b1f82dac24d09d86f8c3eebddc64e80e9c
Natalie Chow <cychownat@gmail.com>
2015-12-25 00:28:07 -0800

    Double sorting/filtering on admin page

--- scripts/adminController.js
+++ scripts/adminController.js
@@ -3,6 +3,8 @@ var adminController = {};
 adminController.showList = function() {
   adminView.listView();
   adminController.handleClaimButtons();
+  adminController.handleSort();
+  adminController.handleStatusDisplay();
 };
 
 adminController.handleClaimButtons = function() {
@@ -11,6 +13,32 @@ adminController.handleClaimButtons = function() {
     console.log('sent ' + $(this).data('key'));
     adminView.sendClaim($(this));
     admin.updateStatusToSent($(this).data('key'));
+  });
+};
+
+adminController.handleSort = function() {
+  $('#admin-sort').on('change', function(event) {
+    event.preventDefault();
+    admin.currentSort = $(this).val();
+    console.log(admin.currentSort);
+    adminView.renderList(admin.sortAndFilter());
+  });
+
+  $('#admin-sort-dir').on('click', function(event) {
+    event.preventDefault();
+    admin.sortAsc = !admin.sortAsc;
+    $('#admin-sort').trigger('change');
+  });
+};
+
+adminController.handleStatusDisplay = function() {
+  $('#admin-status').on('click', 'button', function(event) {
+    event.preventDefault();
+    $(this).siblings().removeClass('selected');
+    $(this).addClass('selected');
+    console.log($(this).text().toUpperCase());
+    admin.currentStatus = $(this).text().toUpperCase();
+    adminView.renderList(admin.sortAndFilter());
   });
 };
 
