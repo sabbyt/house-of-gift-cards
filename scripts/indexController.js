7ff921ee5b3c933f02556dffe13cd71c8fc9843b
Natalie Chow <cychownat@gmail.com>
2015-12-28 12:01:21 -0800

    Changed header and carousel images and captions

---
+++ scripts/indexController.js
@@ -0,0 +1,15 @@
+var indexController = {};
+
+indexController.index = function() {
+  $('#landing-to-submit').on('click', function() {
+    $(location).attr('href', '/request.html');
+  });
+  $('#landing-to-stats').on('click', function() {
+    $(location).attr('href', '/stats.html');
+  });
+  $('#landing-to-login').on('click', function() {
+    $(location).attr('href', '/login.html');
+  });
+};
+
+indexController.index();
